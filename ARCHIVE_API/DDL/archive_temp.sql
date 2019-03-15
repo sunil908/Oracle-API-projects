863f29cb761a4258d67d3e91837dfbe80923ad55
sunil908 <30401081+sunil908@users.noreply.github.com>
2019-03-15 16:51:19 +0530

    Create necessary temp tables for this program
    contains mainly creation of three temp tables
    1. dijnodes : contains tables names
    2. dijpaths: contains all paths between the nodes as relationships
    3. relmap: is temp table used to store intermediate results to run the archive for all the node tables

---
+++ ARCHIVE_API/DDL/archive_temp.sql
@@ -0,0 +1,65 @@
+create or replace PROCEDURE dijcreate
+IS
+BEGIN
+        BEGIN
+            EXECUTE IMMEDIATE 'TRUNCATE TABLE dijnodes' ;
+            EXECUTE IMMEDIATE 'DROP TABLE dijnodes' ;
+        EXCEPTION
+             WHEN OTHERS THEN 
+                dbms_output.put_line('unable to drop the temp table');        
+            END;
+
+
+        BEGIN
+          EXECUTE IMMEDIATE 'TRUNCATE TABLE relmap' ;
+          EXECUTE IMMEDIATE 'DROP TABLE relmap' ;
+        EXCEPTION
+             WHEN OTHERS THEN 
+                dbms_output.put_line('unable to drop the temp table');        
+        END;
+ 
+            
+        BEGIN
+            EXECUTE IMMEDIATE 'TRUNCATE TABLE dijpaths' ;
+            EXECUTE IMMEDIATE 'DROP TABLE dijpaths' ;
+         EXCEPTION
+             WHEN OTHERS THEN 
+                dbms_output.put_line('unable to drop the temp table');        
+         END;
+
+     
+        
+            EXECUTE IMMEDIATE 'CREATE GLOBAL TEMPORARY TABLE  relmap ( 
+				FromRouteNodeID INT, 
+				FromNodeName VARCHAR(200), 
+				ToRouteNodeID INT,
+				ToNodeName VARCHAR(200), 
+				Costs INT ,
+				RelationReferenceID INT
+			)ON COMMIT preserve ROWS';
+
+		
+        
+            EXECUTE IMMEDIATE 'CREATE  GLOBAL TEMPORARY TABLE  dijpaths ( 
+			  PathID NUMBER GENERATED ALWAYS AS IDENTITY,  
+			  FromNodeID int NOT NULL , 
+			  ToNodeID int NOT NULL , 
+			  Costs int NOT NULL,
+			  RelationReferenceID int NULL
+			)ON COMMIT preserve ROWS';
+
+        
+            EXECUTE IMMEDIATE 'CREATE GLOBAL TEMPORARY  TABLE  dijnodes ( 
+			  NodeID NUMBER GENERATED ALWAYS AS IDENTITY, 
+			  Nodename varchar (200) NOT NULL, 
+			  Costs int NULL, 
+			  PathID int NULL, 
+			  Calculated int NULL ,
+              studyroot int DEFAULT 0,
+              tableorder int NULL
+			)ON COMMIT preserve ROWS'; 
+        COMMIT;
+END;
+/
+
+
